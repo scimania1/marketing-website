1bf4d88a6ec8960fd23c894b0613de5e1aafdc20
scimania <gauravdeepubhi@gmail.com>
2024-02-17 23:03:58 +0530

    style: fixed the font for ceo-achievements section

--- src/components/ceo-achievements.tsx
+++ src/components/ceo-achievements.tsx
@@ -13,11 +13,13 @@ export default function CeoAchievements({
   return (
     <>
       <div className="self-center">
-        <h2 className="text-muted-foreground font-bold text-2xl lg:text-3xl">
+        <h2
+          className={`${playfairDisplay.className} text-muted-foreground font-medium text-2xl lg:text-3xl`}
+        >
           {title}
         </h2>
         <h3
-          className={`${playfairDisplay.className} font-bold text-2xl lg:text-3xl`}
+          className={`${playfairDisplay.className} font-medium text-2xl lg:text-3xl`}
         >
           <NumberCount start={1} end={amount} duration={1} />
           {` ${symbol || ""}`}
